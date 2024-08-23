269673f42762f2a7e9cd43e482eefc16a61646b3
Samra4-stack <samratariq4544@gmail.com>
2024-08-22 22:07:52 -0700

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,49 @@
+# Assignment 3
+# Question no 1  (Calculate age)
+birthyear , currentyear = 2005 , 2024 
+age = currentyear - birthyear
+print(age ,"years")
+
+# Question no 2 (Calculate Area of Rectangle)
+length , width = 10 , 20
+AreaofRectangle = length * width 
+print(AreaofRectangle , "m**2")
+
+# Question no 3  (Calculate Area of Circle)
+pie , radius = 3.14 , 5
+AreaofCircle = pie * radius**2
+print(AreaofCircle , "m**2")
+
+# Question no 4 (Calculate Area of CUBE)
+a = 6
+AreaofCube = 6 * a**2
+print(AreaofCube , "m**2")
+
+# Question no 5  (Convert Celsius into fahrenheit & vice versa) 
+Celsius , fahrenheit  = 23 , 5
+C_to_F = (  Celsius * 9/5 ) + 32
+print(C_to_F , "F")
+F_to_C = (fahrenheit - 32) * 5/9
+print(F_to_C , "C")
+
+# Question no 6 (Convert minutes into seconds & vice versa) 
+seconds , minutes = 213 , 15
+minutes_to_seconds = minutes * 60
+print(minutes_to_seconds , "seconds")
+seconds_to_minutes = seconds / 60
+print(seconds_to_minutes , "minutes")
+
+# Question no 7 (Calculate Percentage)
+obtained , total = 992 , 1100
+percentage = obtained / total * 100
+print(percentage , "%")
+
+# Question no 8 (Calculate BMI)
+weight , height = 73 , 10
+BMI = weight / (height * height) 
+print(BMI , "kgm**-2")
+
+# Question no 9 (Volume of cylinder)
+pie , radius , height = 3.14 , 3 , 5
+Volume_of_cylinder = pie * radius**2 * height
+print(Volume_of_cylinder , "m**3")
